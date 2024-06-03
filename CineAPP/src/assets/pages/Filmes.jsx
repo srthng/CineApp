50dd27cb07ec34ba74e0c21ff5880f3ed82f9209
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-03 12:58:08 -0300

    a

--- CineAPP/src/assets/pages/Filmes.jsx
+++ CineAPP/src/assets/pages/Filmes.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 function Filmes() {
+
+    const [filmes, setFilmes] = useState([])
+
+    // const apiKey='7c572a9f5b3ba776080330d23bb76e1e'
+    // const urlBase = 'https://api.themoviedb.org/3/movie/'
+    // const urlImg = 'https://image.tmdb.org/t/p/w342/'
+
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e')
+    .then(response => response.json())
+    .then(response => setFilmes(response))
+    .catch(erro => console.log(erro))
+    
+
+
     return ( 
+        <>
         <h1>Filmes</h1>
-     );
+        {
+            filmes.map(filme => (
+                <div key={filme.id}>
+                <h1>{filme.title}</h1>
+                </div>
+            ))
+        }
+        </>
+     )
 } 
 
 export default Filmes;
--- CineAPP/src/assets/pages/Home.jsx
+++ CineAPP/src/assets/pages/Home.jsx
@@ -1,7 +1,6 @@
 import data from '../../../artigos.json'
 
 function Home() {
-    console.log(data)
     return ( 
         <>
         <input type='text' id="busca" placeholder='Digite sua busca'/>
@@ -10,13 +9,13 @@ function Home() {
             data.map( filme =>(
                 <div className='card' key={filme.title}>
                     <h1> {filme.title} </h1>
-                    <img src={filme.image} alt={filme.title} />
+                    <img className='mb-2' src={filme.image} alt={filme.title} />
                     <div className='tag'>
                         {filme.tags.map(tag =>(
-                            <span key={tag} className='text-purple-500'>{tag}</span>
+                            <span key={tag} className=''>{tag}</span>
                         ))}
                     </div>
-                    <div>
+                    <div className='texto'>
                         {filme.text.map(texto =>(
                             <p key={texto}>{texto}</p>
                         ))}
