35ea6d039364191353347d9d150e7de9cfa37cdb
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 12:56:17 -0300

    a

--- src/pages/Filmes.jsx
+++ src/pages/Filmes.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-function Filmes() {
+
+export default function Filmes() {
+
 
     const [filmes, setFilmes] = useState([])
     const apiKey='api_key=7c572a9f5b3ba776080330d23bb76e1e'
@@ -14,18 +16,20 @@ function Filmes() {
         .then(response => setFilmes(response.results))
         .catch (error => console.log(error))
     },[])
+
+
+
     return ( 
         <>
         <h1>Filmes</h1>
-        <div className="listaFilmes flex flex-row gap-3">
+        <div className="listaFilmes flex flex-row gap-3 flex-wrap">
             {
                 filmes.map(
                     filme => (
                         <div className="card-filme" key={filme.id}>
-                            <h1>{filme.title}</h1>
                             <img src={`${urlImg}${filme.poster_path}`}/>
+                            <h1>{filme.title}</h1>
                             <Link to={`${filme.id}`} className="bg-blue-500">Saber Mais</Link>
-                            {/*<p>{filme.overview}</p>*/}
                         </div>
                     )
                 )
@@ -36,5 +40,3 @@ function Filmes() {
 
      );
 }
-
-export default Filmes ;
--- src/pages/PaginaFilme.jsx
+++ src/pages/PaginaFilme.jsx
@@ -1,9 +1,28 @@
-function PaginaFilmes(){
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+
+export default function PaginaFilmes(){
+
+    const { id } = useParams();
+    const [filme, setFilmes] = useState({});
+
+
+    const apiKey='api_key=7c572a9f5b3ba776080330d23bb76e1e'
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const urlImg = 'https://image.tmdb.org/t/p/w342/'
+
+    useEffect(() => {
+        fetch(`${urlBase}${id}?${apiKey}&language=pt-BR`)
+        .then(response => response.json())
+        .then(response => setFilmes(response))
+        .catch (error => console.log(error))
+    },[])
+
     return(
         <>
-        <h1>Página Filmes</h1>
+        <img src={`${urlImg}${filme.backdrop_path}`}/>
+        <h1>{filme.title}</h1>
+        <p>{filme.overview}</p>
         </>
     )
 }
-
-export default PaginaFilmes()
